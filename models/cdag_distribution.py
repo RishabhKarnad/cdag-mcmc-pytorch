Fix issues related to full covariance optimization

models/cdag_distribution.py
@@ -3,7 +3,7 @@ import scipy.stats as stats
 
 from .clustering_distribution import ClusteringDistribution
 from .graph_distribution import SparseDAGDistribution
-from .cluster_linear_gaussian_network import ClusterLinearGaussianNetwork
+from .cluster_linear_gaussian_network import ClusterLinearGaussianNetwork, ClusterLinearGaussianNetworkFullCov
 from utils.cdag import clustering_to_matrix
 from utils.dataset import CustomDataset
 
@@ -14,18 +14,20 @@ class CDAGJointDistribution(torch.nn.Module):
                  min_clusters,
                  mean_clusters,
                  max_clusters,
-                 optimize_cov,
-                 cluster_prior=ClusteringDistribution,
-                 graph_prior=SparseDAGDistribution,
-                 likelihood=ClusterLinearGaussianNetwork):
+                 optimize_cov=False,
+                 full_covariance=False):
         super().__init__()
         self.n_vars = n_vars
         self.min_clusters = min_clusters
         self.mean_clusters = mean_clusters
         self.max_clusters = max_clusters
-        self.cluster_prior = cluster_prior(n_vars, max_clusters)
-        self.graph_prior = graph_prior(n_vars)
-        self.likelihood = likelihood(n_vars, optimize_cov=optimize_cov)
+        self.cluster_prior = ClusteringDistribution(n_vars, max_clusters)
+        self.graph_prior = SparseDAGDistribution(n_vars)
+        if full_covariance:
+            self.likelihood = ClusterLinearGaussianNetworkFullCov(n_vars)
+        else:
+            self.likelihood = ClusterLinearGaussianNetwork(
+                n_vars, optimize_cov=optimize_cov)
 
     def logpmf(self, C, G, X, batch=False):
         k = len(C)

models/cluster_linear_gaussian_network.py
@@ -29,19 +29,22 @@ class ClusterLinearGaussianNetwork(torch.nn.Module):
 
 
 class ClusterLinearGaussianNetworkFullCov(torch.nn.Module):
-    def __init__(self, n_vars, *, bias=True, Cov=None, optimize_cov=False):
+    def __init__(self, n_vars, *, bias=True):
         super().__init__()
         self.n_vars = n_vars
         self.fc = MaskedLinear(n_vars, n_vars, bias)
-        Cov = Cov or torch.eye(n_vars)
-        self.Cov = torch.nn.Parameter(Cov, requires_grad=optimize_cov)
+        self.L = torch.nn.Parameter(torch.randn((n_vars, n_vars)))
+        self.mask = torch.nn.Parameter(torch.tril(
+            torch.ones((n_vars, n_vars))), requires_grad=False)
 
     def logpmf(self, X, C, G):
         G_expand = C@G@C.T
         mean_expected = self.fc(X, G_expand)
+        L = self.L * self.mask
+        Cov = L@L.T
         return (mv_gauss_log_prob(X.unsqueeze(dim=1),
                                   mean_expected,
-                                  covariance_matrix=self.Cov)
+                                  covariance_matrix=Cov)
                 .mean())
 
     def sample(self, n_samples=1):