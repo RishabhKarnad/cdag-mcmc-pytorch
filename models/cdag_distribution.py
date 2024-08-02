Fix score computation

models/cdag_distribution.py
@@ -42,7 +42,8 @@ class CDAGJointDistribution(torch.nn.Module):
             dataloader = torch.utils.data.DataLoader(
                 dataset, batch_size=64, shuffle=True)
             for _, X_i in enumerate(dataloader):
-                p_d += self.likelihood.logpmf(X_i, C_mat, G)
+                p_d += (self.likelihood.logpmf(X_i, C_mat, G) * X_i.shape[0])
+            p_d /= len(dataset)
         else:
             p_d = self.likelihood.logpmf(X, C_mat, G)
 

models/clustering_distribution.py
@@ -10,7 +10,7 @@ class ClusteringDistribution(torch.nn.Module):
         self.lambda_s = lambda_s
 
     def logpmf(self, C):
-        return self.lambda_s * torch.prod(torch.tanh(self.alpha * torch.sum(C, axis=0)))
+        return -self.lambda_s * torch.prod(torch.tanh(self.alpha * torch.sum(C, axis=0)))
 
     def sample(self, n_samples=1):
         raise NotImplementedError

samplers/cdag_mcmc_sampler.py
@@ -68,7 +68,7 @@ class ClusteringProposalDistribution:
                     self.neighbour_counts.append(
                         (comb(len(self.C[i]), c1) * comb(len(self.C[i+1]), c2)))
 
-        self.total_neighbours = self.neighbour_counts[-1]
+        self.total_neighbours = np.sum(self.neighbour_counts)
 
     def gen_neighbour(self, spec):
         neighbour = deepcopy(self.C)
@@ -121,7 +121,7 @@ class GraphProposalDistribution:
         m, m = G.shape
         self.n_nodes = m
 
-        self.total_neighbours = m*(m-1) / 2
+        self.total_neighbours = m*(m-1) / 2 + 1
 
     def sample(self):
         """